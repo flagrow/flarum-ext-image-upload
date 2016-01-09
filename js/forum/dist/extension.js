b6475aa8e8b7245f41b9f7ed0c258a9d78fedbcb
Matteo Pompili <matpompili@gmail.com>
2016-01-09 12:17:09 +0100

    transpiled

---
+++ js/forum/dist/extension.js
@@ -0,0 +1,212 @@
+System.register('flagrow/remote-image-upload/components/UploadButton', ['flarum/Component', 'flarum/helpers/icon'], function (_export) {
+    'use strict';
+
+    var Component, icon, UploadButton;
+    return {
+        setters: [function (_flarumComponent) {
+            Component = _flarumComponent['default'];
+        }, function (_flarumHelpersIcon) {
+            icon = _flarumHelpersIcon['default'];
+        }],
+        execute: function () {
+            UploadButton = (function (_Component) {
+                babelHelpers.inherits(UploadButton, _Component);
+
+                function UploadButton() {
+                    babelHelpers.classCallCheck(this, UploadButton);
+                    babelHelpers.get(Object.getPrototypeOf(UploadButton.prototype), 'constructor', this).apply(this, arguments);
+                }
+
+                babelHelpers.createClass(UploadButton, [{
+                    key: 'init',
+
+                    /**
+                     * Load the configured remote uploader service.
+                     */
+                    value: function init() {
+                        // the service type handling uploads
+                        this.type = app.forum.attribute('flagrow.remote-image-upload.type') || 'oauth';
+                    }
+
+                    /**
+                     * Show the actual Upload Button.
+                     *
+                     * @returns {*}
+                     */
+                }, {
+                    key: 'view',
+                    value: function view() {
+                        return m('div', { className: 'Button hasIcon flagrow-image-upload-button' }, [icon('paperclip', { className: 'Button-icon' }), m('span', { className: 'Button-label' }, app.translator.trans('flagrow-remote-image-upload.forum.buttons.attach')), m('input', {
+                            type: 'file',
+                            accept: 'image/*',
+                            name: 'flagrow-image-upload-input',
+                            onchange: this.process.bind(this)
+                        })]);
+                    }
+
+                    /**
+                     * Process the upload event.
+                     */
+                }, {
+                    key: 'process',
+                    value: function process() {
+
+                        // show loader
+                        this.markLoaderStarted();
+
+                        console.log(this);
+
+                        //run the service (oauth or the user specificed one)
+                        this[this.type]();
+                    }
+                }, {
+                    key: 'oauth',
+                    value: function oauth() {
+                        // api endpoint
+                        this.endpoint = app.forum.attribute('flagrow.remote-image-upload.endpoint');
+                        // client id
+                        this.client_id = app.forum.attribute('flagrow.remote-image-upload.client_id');
+                        // client bearer token if non-anonymous
+                        this.token = app.forum.attribute('flagrow.remote-image-upload.token') || null;
+                        // whether uploading is anonymous, not account bound
+                        this.isAnonymous = app.forum.attribute('flagrow.remote-image-upload.anonymous') || true;
+
+                        if (this.isAnonymous || !this.token) {
+                            var headers = {
+                                client_id: this.client_id
+                            };
+                        } else {
+                            var headers = {
+                                bearer: this.token
+                            };
+                        }
+
+                        var button = this;
+
+                        $.ajax(this.endpoint, {
+                            method: 'post',
+                            // authorization and other headers
+                            headers: headers,
+                            // upload success
+                            success: function success(payload, statusCode, xhr) {
+
+                                button.markLoaderSuccess();
+                            },
+                            // upload error
+                            error: function error(xhr, statusText, _error) {
+
+                                button.markLoaderFailed();
+                            },
+                            statusCode: {
+                                // unauthorized
+                                401: function _() {}
+                            }
+                        });
+                    }
+
+                    /**
+                     * Sets the icon classes of the icon in the upload input field.
+                     *
+                     * @param classes
+                     */
+                }, {
+                    key: 'setIconClasses',
+                    value: function setIconClasses(classes) {
+                        this.$('i').removeClass('fa-paperclip fa-spin fa-circle-o-notch fa-check green fa-times red').addClass(classes);
+                    }
+
+                    /**
+                     * Sets the label of the uploader button and allows disabling submits.
+                     *
+                     * @param text
+                     * @param disable
+                     */
+                }, {
+                    key: 'setLabel',
+                    value: function setLabel(text) {
+                        var disable = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
+
+                        this.$('.Button-label').text(text);
+
+                        // enable on timeout
+                        if (disable === false && $('.item-submit > button').attr('disabled') === true) {
+                            setTimeout(function () {
+                                $('.item-submit > button').attr('disabled', false);
+                            }, 1000);
+                        } else if (disable === true) {
+                            $('.item-submit > button').attr('disabled', true);
+                        }
+                    }
+
+                    /**
+                     * Modifies the file upload button to indicate upload started.
+                     */
+                }, {
+                    key: 'markLoaderStarted',
+                    value: function markLoaderStarted() {
+                        this.setIconClasses('fa-spin fa-circle-o-notch');
+                        this.setLabel(app.translator.trans('flagrow-remote-image-upload.forum.states.loading'), true);
+                    }
+
+                    /**
+                     * Modifies the file upload button to indicate upload success.
+                     */
+                }, {
+                    key: 'markLoaderSuccess',
+                    value: function markLoaderSuccess() {
+                        this.setIconClasses('fa-check green');
+                        this.setLabel(app.translator.trans('flagrow-remote-image-upload.forum.states.success'), false);
+                    }
+
+                    /**
+                     * Modifies the file upload button to indicate upload failed.
+                     */
+                }, {
+                    key: 'markLoaderFailed',
+                    value: function markLoaderFailed() {
+                        this.setIconClasses('fa-times red');
+                        this.setLabel(app.translator.trans('flagrow-remote-image-upload.forum.states.error'), false);
+                    }
+
+                    /**
+                     * Resets the file upload button to its original state.
+                     */
+                }, {
+                    key: 'resetLoader',
+                    value: function resetLoader() {
+                        this.setIconClasses('fa-paperclip');
+                    }
+                }]);
+                return UploadButton;
+            })(Component);
+
+            _export('default', UploadButton);
+        }
+    };
+});;
+System.register('flagrow/remote-image-upload/main', ['flarum/extend', 'flarum/components/TextEditor', 'flagrow/remote-image-upload/components/UploadButton'], function (_export) {
+    'use strict';
+
+    var extend, TextEditor, UploadButton;
+    return {
+        setters: [function (_flarumExtend) {
+            extend = _flarumExtend.extend;
+        }, function (_flarumComponentsTextEditor) {
+            TextEditor = _flarumComponentsTextEditor['default'];
+        }, function (_flagrowRemoteImageUploadComponentsUploadButton) {
+            UploadButton = _flagrowRemoteImageUploadComponentsUploadButton['default'];
+        }],
+        execute: function () {
+
+            app.initializers.add('flagrow-remote-image-upload', function (app) {
+
+                /**
+                 * Add the upload button to the post composer.
+                 */
+                extend(TextEditor.prototype, 'controlItems', function (items) {
+                    items.add('flarum-remote-image-upload', new UploadButton(), 20);
+                });
+            });
+        }
+    };
+});
