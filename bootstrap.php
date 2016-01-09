77deceb0a1574271a801256505ace44e0d86238d
Matteo Pompili <matpompili@gmail.com>
2016-01-09 15:48:11 +0100

    Make settings available in forum js

--- bootstrap.php
+++ bootstrap.php
@@ -6,4 +6,5 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
     $events->subscribe(Listeners\AddClientAssets::class);
+    $events->subscribe(Listeners\LoadSettingsFromDatabase::class);
 };
---
+++ src/Listeners/LoadSettingsFromDatabase.php
@@ -0,0 +1,51 @@
+<?php namespace Flagrow\RemoteImageUpload\Listeners;
+
+/*
+* This file is part of remote-image-upload.
+*
+* (c) Flagrow
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+use Flarum\Settings\SettingsRepositoryInterface;
+use Illuminate\Contracts\Events\Dispatcher;
+use Flarum\Event\PrepareApiAttributes;
+use Flarum\Api\Serializer\ForumSerializer;
+
+class LoadSettingsFromDatabase {
+  protected $settings;
+
+  /**
+  * Gets the settings variable. Called on Object creation.
+  *
+  * @param SettingsRepositoryInterface $settings
+  */
+  public function __construct(SettingsRepositoryInterface $settings) {
+    $this->settings = $settings;
+  }
+
+  /**
+  * Subscribes to the Flarum events.
+  *
+  * @param Dispatcher $events
+  */
+  public function subscribe(Dispatcher $events) {
+    $events->listen(PrepareApiAttributes::class, [$this, 'prepareApiAttributes']);
+  }
+  /**
+  * Get the setting values from the database and make them available
+  * in the forum.
+  *
+  * @param PrepareApiAttributes $event
+  */
+  public function prepareApiAttributes(PrepareApiAttributes $event) {
+    if ($event->isSerializer(ForumSerializer::class)) {
+      $event->attributes['flagrow.remote-image-upload.endpoint'] = $this->settings->get('flagrow.remote-image-upload.endpoint');
+      $event->attributes['flagrow.remote-image-upload.client_id'] = $this->settings->get('flagrow.remote-image-upload.client_id');
+    //   $event->attributes['flagrow.remote-image-upload.max_width'] = $this->settings->get('flagrow.remote-image-upload.max_width');
+    //   $event->attributes['flagrow.remote-image-upload.max_height'] = $this->settings->get('flagrow.remote-image-upload.max_height');
+    }
+  }
+}
