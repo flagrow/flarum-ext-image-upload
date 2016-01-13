e43899227332c1199e6889f7bb49712704b19f8e
Daniel Klabbers <daniel+hyn@klabbers.email>
2016-01-13 14:39:17 +0100

    added service provider

---
+++ src/Providers/StorageServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of flagrow/flarum-ext-image-upload.
+ *
+ * Copyright (c) Flagrow.
+ *
+ * http://flagrow.github.io
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Flagrow\ImageUpload\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class StorageServiceProvider extends ServiceProvider {
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
