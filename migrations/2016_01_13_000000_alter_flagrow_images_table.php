974d2d01a50bc9cbb025cfe3cd2e681da05be941
Daniel Klabbers <daniel+hyn@klabbers.email>
2016-02-25 10:50:14 +0100

    changes needed for beta-5, migrations use closure array and composer version constraint

--- migrations/2016_01_13_000000_alter_flagrow_images_table.php
+++ migrations/2016_01_13_000000_alter_flagrow_images_table.php
@@ -10,22 +10,13 @@
  * file that was distributed with this source code.
  */
 
-namespace flagrow\image\upload\Migration;
 
-use Flarum\Database\AbstractMigration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
 
-class AlterFlagrowImagesTable extends AbstractMigration
-{
-
-    /**
-     * Run the migrations.
-     *
-     * @info Called when extension is enabled. Never runs twice.
-     */
-    public function up()
-    {
-        $this->schema->table('flagrow_images', function (Blueprint $table) {
+return [
+    'up' => function(Builder $schema) {
+        $schema->table('flagrow_images', function (Blueprint $table) {
             // the specific post id this image is appearing in
             $table->dropColumn('post_id');
             // file url of the image
@@ -33,19 +24,12 @@ class AlterFlagrowImagesTable extends AbstractMigration
             // file size of the image
             $table->integer('file_size')->default(0);
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @info Called using the uninstall button in the admin.
-     */
-    public function down()
-    {
-        $this->schema->table('flagrow_images', function (Blueprint $table) {
+    },
+    'down' => function(Builder $schema) {
+        $schema->table('flagrow_images', function (Blueprint $table) {
             $table->dropColumn('file_url');
             $table->dropColumn('file_size');
             $table->integer('post_id')->unsigned();
         });
     }
-}
+];
